63af5b52ddc493c0268899e29ffaf25059a54de3
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2025-11-07 12:35:32 +0200

    Push third function to remote repo

--- main.py
+++ main.py
@@ -68,7 +68,15 @@ def factorial(n:int):
     n: non-negative integer
     return: n!
     """
-    pass
+    if n == 0 or n == 1:
+        return 1
+    
+    result = 1
+
+    for i in range(2, n + 1):
+        result = result * i
+
+    return result
     
 def bar_graph()->str:
     """
